56fbe1188c52aad58c24784b8fd508880aa21d4d
GitLeoDevHub <169077143+GitLeoDevHub@users.noreply.github.com>
2024-05-13 18:16:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const sel = document.querySelector('#sel')
-const Input = document.querySelector('.input')
+const rubInput = document.querySelector('#rub')
+const eurInput = document.querySelector('#eur')
+const usdInput = document.querySelector('#usd')
+const cnyInput = document.querySelector('#cny')
 const startBtn = document.querySelector('#start')
 const usdValue = document.querySelector('#usd_value')
 const eurValue = document.querySelector('#eur_value')
@@ -19,30 +21,25 @@ const getData = async (url) => {
 startBtn.addEventListener('click', () => {
     getData('https://www.cbr-xml-daily.ru/latest.js')
         .then((courses) => {
-            if (sel.value = "rub") {
-                usdValue.textContent = `USD ${(Input.value * courses.rates.USD).toFixed(2)}$`
-                eurValue.textContent = `EUR ${(Input.value * courses.rates.EUR).toFixed(2)}€`
-                cnyValue.textContent = `CNY ${(Input.value * courses.rates.CNY).toFixed(2)}¥`
-                rubValue.textContent = `RUB ${Input.value}₽`
-            }
-            if (sel.value = "eur") {
-                usdValue.textContent = `USD ${(Input.value * courses.rates.USD).toFixed(2)}$`
-                eurValue.textContent = `EUR ${Input.value}€`
-                cnyValue.textContent = `CNY ${(Input.value * courses.rates.CNY).toFixed(2)}¥`
-                rubValue.textContent = `RUB ${(Input.value / courses.rates.EUR).toFixed(2)}₽`
-            }
-            if (sel.value = "usd") {
-                usdValue.textContent = `USD ${Input.value}$`
-                eurValue.textContent = `EUR ${(Input.value * courses.rates.EUR).toFixed(2)}€`
-                cnyValue.textContent = `CNY ${(Input.value * courses.rates.CNY).toFixed(2)}¥`
-                rubValue.textContent = `RUB ${(Input.value / courses.rates.RUB).toFixed(2)}₽`
-            }
-            if (sel.value = "cny") {
-                usdValue.textContent = `USD ${(Input.value * courses.rates.USD).toFixed(2)}$`
-                eurValue.textContent = `EUR ${(Input.value * courses.rates.EUR).toFixed(2)}€`
-                cnyValue.textContent = `CNY ${Input.value}¥`
-                rubValue.textContent = `RUB ${(Input.value * courses.rates.RUB).toFixed(2)}₽`
-            }
 
+            usdValue.textContent = `USD ${(rubInput.value * courses.rates.USD).toFixed(2)} $`
+            eurValue.textContent = `EUR ${(rubInput.value * courses.rates.EUR).toFixed(2)} €`
+            cnyValue.textContent = `CNY ${(rubInput.value * courses.rates.CNY).toFixed(2)} ¥`
+            rubValue.textContent = `RUB ${(rubInput.value).toFixed(2)} ₽`
+
+            usdValue.textContent = `USD ${(eurInput.value * courses.rates.USD).toFixed(2)} $`
+            eurValue.textContent = `EUR ${(eurInput.value * courses.rates.EUR).toFixed(2)} €`
+            cnyValue.textContent = `CNY ${(eurInput.value * courses.rates.CNY).toFixed(2)} ¥`
+            rubValue.textContent = `RUB ${(eurInput.value).toFixed(2)} ₽`
+
+            usdValue.textContent = `USD ${(rubInput.value * courses.rates.USD).toFixed(2)} $`
+            eurValue.textContent = `EUR ${(rubInput.value * courses.rates.EUR).toFixed(2)} €`
+            cnyValue.textContent = `CNY ${(rubInput.value * courses.rates.CNY).toFixed(2)} ¥`
+            rubValue.textContent = `RUB ${(rubInput.value).toFixed(2)} ₽`
+
+            usdValue.textContent = `USD ${(rubInput.value * courses.rates.USD).toFixed(2)} $`
+            eurValue.textContent = `EUR ${(rubInput.value * courses.rates.EUR).toFixed(2)} €`
+            cnyValue.textContent = `CNY ${(rubInput.value * courses.rates.CNY).toFixed(2)} ¥`
+            rubValue.textContent = `RUB ${(rubInput.value).toFixed(2)} ₽`
         })
 })
